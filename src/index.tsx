370172e4eccf9da6cc895985c07644db0d7c5779
Kasperoid <ivanu9053@gmail.com>
2024-09-12 23:27:33 +0300

    Refactoring app and index components

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './styles/index.css';
+import 'normalize.css';
+import { ConfigProvider } from 'antd';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <ConfigProvider
+    theme={{
+      token: {
+        colorPrimary: '#2bd1d1',
+        colorInfo: '#2bd1d1',
+        colorTextBase: '#b2b1b8',
+        colorLink: '#b2b1b8',
+        colorLinkHover: '#26252a',
+        colorTextSecondary: '#9e9aa7',
+        colorTextTertiary: '#35323e',
+        colorTextQuaternary: '#232127',
+        colorText: '#b2b1b8',
+        borderRadius: 14,
+        colorPrimaryHover: '#9be3e2',
+      },
+      components: {
+        Button: {
+          paddingInlineLG: 30,
+          paddingBlockLG: 22,
+          paddingBlockSM: 17,
+          paddingInlineSM: 25,
+        },
+      },
+    }}
+  >
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ConfigProvider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
