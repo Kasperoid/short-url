3a297deb21d7d851809b7b6c74ca1f5292694068
Kasperoid <ivanu9053@gmail.com>
2024-09-19 11:57:16 +0300

    Добавление стилей к интро

---
+++ src/styles/content/intro/IntroRow.tsx
@@ -0,0 +1,18 @@
+import { Row, RowProps } from 'antd';
+import { styled } from 'styled-components';
+import { baseTheme } from '../../theme';
+
+export const IntroRow = styled(Row)<RowProps>`
+  && {
+    padding: 60px 0 120px 0;
+
+    @media (${baseTheme.size.media.sm}) {
+      padding: 20px 0 100px 0;
+      gap: 15px;
+    }
+
+    @media (${baseTheme.size.media.xs}) {
+      padding: 20px 0 140px 0;
+    }
+  }
+`;
