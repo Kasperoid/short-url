2439baf8b91ed451c854733ff1123b633a67fdd8
Kasperoid <ivanu9053@gmail.com>
2024-09-19 11:42:57 +0300

    Добавление стилей для сохраненных ссылок

---
+++ src/styles/content/saveInput/SaveInputCardStyled.tsx
@@ -0,0 +1,23 @@
+import { Flex, FlexProps } from 'antd';
+import { styled } from 'styled-components';
+import { baseTheme } from '../../theme';
+
+export const SaveInputCardStyled = styled(Flex)<FlexProps>`
+  && {
+    padding: 10px 25px;
+    background-color: white;
+    border-radius: ${baseTheme.border.radius.round}px;
+
+    @media (${baseTheme.size.media.sm}) {
+      align-items: start;
+      flex-direction: column;
+      min-width: 400px;
+      padding: 10px;
+      margin: 0 auto;
+    }
+
+    @media (${baseTheme.size.media.xs}) {
+      min-width: 100%;
+    }
+  }
+`;
---
+++ src/styles/content/saveInput/SaveLinkContainerStyled.tsx
@@ -0,0 +1,22 @@
+import { Flex, FlexProps } from 'antd';
+import { styled } from 'styled-components';
+import { baseTheme } from '../../theme';
+
+export const SaveLinkContainerStyled = styled(Flex)<FlexProps>`
+  && {
+    gap: 20px;
+    @media (${baseTheme.size.media.sm}) {
+      border-top: 1px solid ${baseTheme.bg.color.tertiary};
+      flex-direction: column;
+      align-items: stretch;
+      gap: 5px;
+      align-items: start;
+      width: 100%;
+      margin-top: 5px;
+
+      & .ant-btn-primary {
+        width: 100%;
+      }
+    }
+  }
+`;
