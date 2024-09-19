2a21d3b5ad722b8fcb24afe022ce5d5e721cd029
Kasperoid <ivanu9053@gmail.com>
2024-09-19 20:55:37 +0300

    Корневые компоненты

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
-import './styles/global/global.css';
 import { Layout } from 'antd';
 import HeaderContainer from './components/Header/HeaderContainer';
 import IntroContainer from './components/Content/Intro/IntroContainer';
 import MainContentCotainer from './components/Content/MainContent/MainContentContainer';
+import FooterContainer from './components/Footer/FooterContainer';
 
 function App() {
   const { Content } = Layout;
   return (
     <Layout
       style={{
-        height: '100vh',
         backgroundColor: 'white',
+        paddingTop: '30px',
       }}
     >
       <HeaderContainer />
@@ -19,6 +19,7 @@ function App() {
         <IntroContainer />
         <MainContentCotainer />
       </Content>
+      <FooterContainer />
     </Layout>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,41 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './styles/index.css';
 import 'normalize.css';
-import { ConfigProvider } from 'antd';
+import { ThemeProvider } from 'styled-components';
+import { baseTheme } from './styles/theme';
+import GlobalStyles from './styles/global';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <ConfigProvider
-    theme={{
-      token: {
-        colorPrimary: '#2bd1d1',
-        colorInfo: '#2bd1d1',
-        colorTextBase: '#b2b1b8',
-        colorLink: '#b2b1b8',
-        colorLinkHover: '#26252a',
-        colorTextSecondary: '#9e9aa7',
-        colorTextTertiary: '#35323e',
-        colorTextQuaternary: '#232127',
-        colorText: '#b2b1b8',
-        borderRadius: 14,
-        colorPrimaryHover: '#9be3e2',
-      },
-      components: {
-        Button: {
-          paddingInlineLG: 30,
-          paddingBlockLG: 22,
-          paddingBlockSM: 17,
-          paddingInlineSM: 25,
-        },
-      },
-    }}
-  >
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </ConfigProvider>
+  <React.StrictMode>
+    <ThemeProvider theme={baseTheme} />
+    <GlobalStyles />
+    <App />
+  </React.StrictMode>
 );
