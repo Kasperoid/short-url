87ce34a819016708f5fc6ab538c16ce878516eb8
Kasperoid <ivanu9053@gmail.com>
2024-09-19 20:48:12 +0300

    Стилизация текста разного формата

---
+++ src/styles/TextStyled.tsx
@@ -0,0 +1,35 @@
+import { Typography } from 'antd';
+import { TextProps } from 'antd/es/typography/Text';
+import { styled } from 'styled-components';
+import {
+  baseTheme,
+  TextColorType,
+  TextSizeType,
+  TextWeightType,
+} from './theme';
+
+const { Text } = Typography;
+
+interface TextStyledProps extends Omit<TextProps, 'size' | 'color'> {
+  size: keyof TextSizeType;
+  color?: keyof TextColorType;
+  weight?: keyof TextWeightType;
+  lineheight?: number;
+}
+
+export const TextStyled = styled(Text)<TextStyledProps>`
+  && {
+    ${({ lineheight }) => lineheight && `line-height: ${lineheight}`};
+    font-weight: ${({ weight }) =>
+      weight ? baseTheme.font.weight[weight] : 500};
+    color: ${({ color }) =>
+      color ? baseTheme.font.color[color] : baseTheme.font.color.second};
+    ${({ size }) =>
+      size &&
+      `font-size: calc(10px + ${
+        baseTheme.font.size.text[size] - 10
+      } * (100vw - ${baseTheme.size.container.min}px) / ${
+        baseTheme.size.container.max
+      })`}
+  }
+`;
