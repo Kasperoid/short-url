075ecdc9fc67006f81409d5138a06978438ffb68
Kasperoid <ivanu9053@gmail.com>
2024-09-19 20:51:05 +0300

    Создание компоненты для кнопок меню

---
+++ src/components/Header/HeaderMenuNav.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Flex } from 'antd';
+import { MenuOutlined } from '@ant-design/icons';
+import { HeaderBurgerMenuStyled } from '../../styles/header/HeaderBurgerMenuStyled';
+import { ButtonBurgerContainerStyled } from '../../styles/header/ButtonBurgerContainerStyled';
+import { HeaderMenuDescButtonStyled } from '../../styles/header/HeaderMenuDescButtonStyled';
+import { ButtonMenuContainer } from '../../styles/header/ButtonMenuContainer';
+import { ButtonLinkStyled } from '../../styles/ButtonLinkStyled';
+import { LogoIconStyled } from '../../styles/LogoIconStyled';
+
+interface HeaderMenuNavProps {
+  setOpenMenu: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const HeaderMenuNav = ({ setOpenMenu }: HeaderMenuNavProps) => {
+  const options: string[] = ['Features', 'Resources'];
+  return (
+    <Flex flex={1} gap={25}>
+      <LogoIconStyled fillcolor={'secondaryDark'} />
+      <ButtonMenuContainer flex={1}>
+        <HeaderMenuDescButtonStyled gap={25}>
+          {options.map((item, index) => (
+            <ButtonLinkStyled
+              weight="semiBold"
+              size={'mainText'}
+              type="link"
+              key={index + 1}
+            >
+              {item}
+            </ButtonLinkStyled>
+          ))}
+        </HeaderMenuDescButtonStyled>
+        <ButtonBurgerContainerStyled>
+          <HeaderBurgerMenuStyled
+            icon={<MenuOutlined />}
+            onClick={() => setOpenMenu((prevState) => !prevState)}
+          />
+        </ButtonBurgerContainerStyled>
+      </ButtonMenuContainer>
+    </Flex>
+  );
+};
+
+export default HeaderMenuNav;
