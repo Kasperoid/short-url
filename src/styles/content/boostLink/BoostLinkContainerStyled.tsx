d9098bf6dfd7a1a67180f22fe7aba61d068ffef7
Kasperoid <ivanu9053@gmail.com>
2024-09-19 20:47:00 +0300

    Создание стилей для блока с boost link

---
+++ src/styles/content/boostLink/BoostLinkContainerStyled.tsx
@@ -0,0 +1,19 @@
+import { Space, SpaceProps } from 'antd';
+import { styled } from 'styled-components';
+import { baseTheme } from '../../theme';
+
+interface BoostLinkContainerProps extends SpaceProps {
+  bg: React.FunctionComponent<React.SVGAttributes<SVGElement>>;
+}
+
+export const BoostLinkContainerStyled = styled(Space)<BoostLinkContainerProps>`
+  && {
+    background-color: ${baseTheme.bg.color.secondary};
+    width: 100%;
+    text-align: center;
+    padding: 50px 0;
+    background-image: ${({ bg }) => `url(${bg})`};
+    background-repeat: no-repeat;
+    background-size: cover;
+  }
+`;
