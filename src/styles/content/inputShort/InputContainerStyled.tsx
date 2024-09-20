d690d38242eab73a45c6b1a94f75992b0a2fc971
Kasperoid <ivanu9053@gmail.com>
2024-09-20 15:20:42 +0300

    Изменение стилей в контейнере с вводом, увеличен внешний верхний отступ

--- src/styles/content/inputShort/InputContainerStyled.tsx
+++ src/styles/content/inputShort/InputContainerStyled.tsx
@@ -3,6 +3,6 @@ import { styled } from 'styled-components';
 export const InputContainerStyled = styled.div`
   && {
     position: relative;
-    margin-bottom: 70px;
+    margin-bottom: 90px;
   }
 `;
